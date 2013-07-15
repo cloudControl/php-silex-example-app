5249715266a8a44e45f12ceac56faceb7f0a4b81
Dimitris Verraros <vervas.g@gmail.com>
2013-07-15 16:17:19 +0200

    Update SilexAssetic extension to the latest version - refactor index.php to work with the Assetic new vesrion

--- web/index.php
+++ web/index.php
@@ -10,16 +10,20 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__ . '/../resources/twig'
 ));
 
-$app->register(new SilexAssetic\AsseticExtension(), array(
-    'assetic.path_to_web' => __DIR__,
-    'assetic.options' => array(
+$app->register(new SilexAssetic\AsseticServiceProvider());
+
+$app['assetic.path_to_web'] = __DIR__;
+$app['assetic.options'] = array(
         'auto_dump_assets' => true,
         'debug' => false
-    ),
-    'assetic.filters' => $app->protect(function($fm) {
+    );
+$app['assetic.filter_manager'] = $app->share(
+    $app->extend('assetic.filter_manager', function($fm, $app) {
         $fm->set('cssmin', new Assetic\Filter\CssMinFilter());
+
+        return $fm;
     })
-));
+);
 
 $app->get('/', function() use ($app) {
     return $app['twig']->render('hello.twig');
